9e200e04cc8f5fd55ffe46ad252cbbb1652ff10e
AutocodeStudent6 <109426026+AutocodeStudent6@users.noreply.github.com>
2022-07-17 14:55:45 +0500

    Update python_test.py
    commented

--- python_test.py
+++ python_test.py
@@ -1,29 +1,29 @@
-import pytest
+# import pytest
 
-error
-@pytest.fixture(scope='module')
-def call_me_once_use_when_needed():
-    print('\ncall me once use when needed')
-
-
-@pytest.fixture()
-def call_me_every_time():
-    print('call me every time')
+# error
+# @pytest.fixture(scope='module')
+# def call_me_once_use_when_needed():
+#     print('\ncall me once use when needed')
 
 
-@pytest.fixture(autouse=True)
-def call_me_everywhere():
-    print('You\'ll call me even if you don\'t wanna to')
+# @pytest.fixture()
+# def call_me_every_time():
+#     print('call me every time')
 
 
-def test_one(call_me_once_use_when_needed, call_me_every_time):
-    print('test one')
-    print('after chenge by Students6')
+# @pytest.fixture(autouse=True)
+# def call_me_everywhere():
+#     print('You\'ll call me even if you don\'t wanna to')
 
 
-def test_two(call_me_once_use_when_needed, call_me_every_time):
-    print('test two')
-    print('after chenge by Students6')
+# def test_one(call_me_once_use_when_needed, call_me_every_time):
+#     print('test one')
+#     print('after chenge by Students6')
 
-def test_three_by_student6(call_me_once_use_when_needed, call_me_every_time):
-    print('test three by Student6')
+
+# def test_two(call_me_once_use_when_needed, call_me_every_time):
+#     print('test two')
+#     print('after chenge by Students6')
+
+# def test_three_by_student6(call_me_once_use_when_needed, call_me_every_time):
+#     print('test three by Student6')
