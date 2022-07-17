51ee3c0c695b1eb24ef198a7695a3ccd08d9dd64
AutocodeStudent6 <109426026+AutocodeStudent6@users.noreply.github.com>
2022-07-17 14:47:55 +0500

    Update python_test.py
    chenged

--- python_test.py
+++ python_test.py
@@ -18,7 +18,10 @@ def call_me_everywhere():
 
 def test_one(call_me_once_use_when_needed, call_me_every_time):
     print('test one')
+    print('after chenge by Students6')
 
 
 def test_two(call_me_once_use_when_needed, call_me_every_time):
     print('test two')
+    print('after chenge by Students6')
+
